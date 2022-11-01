cea844484abbf2fb1b28fabb93c0f0bb1cffdff2
Lucas Braga <lucashenbr@gmail.com>
2022-10-31 22:10:51 -0300

    link do swagger na homepage

--- online-bank/src/main/java/lucas/online_bank/HomeController.java
+++ online-bank/src/main/java/lucas/online_bank/HomeController.java
@@ -1,11 +1,13 @@
 package lucas.online_bank;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class HomeController {
-
+    @GetMapping()
+    public String linkToSwagger(){
+    return "http://localhost:8080/swagger-ui/index.html?configUrl=/v3/api-docs";
+    }
 }
